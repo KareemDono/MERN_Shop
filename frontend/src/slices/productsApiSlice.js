ef48bfd82d29301d8d2c99c3b8104c2f703834e3
KareemDono <kareemjumaa16@gmail.com>
2023-07-04 02:06:29 +0300

    Loader & Messages

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -4,10 +4,17 @@ import { apiSlice } from "./apiSlice";
 export const productsApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getProducts: builder.query({
-            query: () => ({ url: PRODUCTS_URL }),
+            query: () => ({
+                 url: PRODUCTS_URL
+            }),
+            keepUnusedDataFor: 5
         }),
-        keepUnusedDataFor: 5
+        getProductDetails: builder.query({
+        query: (productId) => ({
+            url: `${PRODUCTS_URL}/${productId}`,
+        }),
+        }),
     }),
 });
 
-export const { useGetProductsQuery } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
