6d063643222df3a647769980c4d32cd4d3957211
KareemDono <kareemjumaa16@gmail.com>
2023-07-06 03:35:15 +0300

    backend committing --all

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,4 +1,3 @@
-//import/destroy data
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import colors from 'colors';
@@ -29,7 +28,7 @@ const importData = async () => {
 
     await Product.insertMany(sampleProducts);
 
-    console.log('Data has been imported successfully.'.green.inverse);
+    console.log('Data Imported!'.green.inverse);
     process.exit();
   } catch (error) {
     console.error(`${error}`.red.inverse);
@@ -43,7 +42,7 @@ const destroyData = async () => {
     await Product.deleteMany();
     await User.deleteMany();
 
-    console.log('Data Destroyed successfully.'.red.inverse);
+    console.log('Data Destroyed!'.red.inverse);
     process.exit();
   } catch (error) {
     console.error(`${error}`.red.inverse);
